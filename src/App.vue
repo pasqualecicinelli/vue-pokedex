014a3ff2de055492a80831f0ff341ee8a2b55ba4
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2024-02-26 19:41:21 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import Search from "./components/Search.vue";
+
+export default {
+  data() {
+    return {};
+  },
+  components: { Search },
+};
+</script>
+
+<template>
+  <header>pokedex</header>
+
+  <main>
+    <div class="container">
+      <Search />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+header {
+  height: 80px;
+  border: 1px solid black;
+  text-align: center;
+  line-height: 80px;
+}
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,53 @@
+<script>
+export default {
+  data() {
+    return {
+      searchTerm: "",
+      baseUrl: "https://pokeapi.co/api/v2/pokemon/",
+    };
+  },
+  methods: {
+    async searchPokemon() {
+      try {
+        // const response = await fetch(
+        //   `https://pokeapi.co/api/v2/pokemon/${this.searchTerm.toLowerCase()}`
+        //   );
+        const response = await fetch(
+          this.baseUrl + this.searchTerm.toLowerCase()
+        );
+
+        if (!response.ok) {
+          throw new Error("Pokémon non trovato!");
+        }
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.error(error.message);
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <main>
+    <div class="mt-5">
+      <input
+        type="text"
+        v-model="searchTerm"
+        placeholder="Inserisci il nome del Pokémon"
+      />
+      <button @click="searchPokemon">Cerca</button>
+    </div>
+
+    <div class="card mt-5" style="width: 18rem; height: 18rem">
+      <!-- <img class="card-img-top" :src="xx" alt="" /> -->
+    </div>
+  </main>
+</template>
+
+<style scoped>
+input {
+  width: 300px;
+}
+</style>
